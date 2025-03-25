3738a28b8ce483579139a851d417a891c90db946
DelaraJ <delara.jalali.382@gmail.com>
2025-03-25 18:53:24 +0330

    feature: add toast configuration in the app scope #6

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
+
   return (
     <div className="App">
       <AppRouter />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
