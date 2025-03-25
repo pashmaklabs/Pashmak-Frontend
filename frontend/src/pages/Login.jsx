c0bd6faadb04f47b41f9a517e5ecd1a02c252471
DelaraJ <delara.jalali.382@gmail.com>
2025-03-25 18:25:06 +0330

    feature: set jwt in localStorage #6

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,6 +6,7 @@ import VerificationCode from "../components/VerificationCode";
 import PasswordLogin from "../components/PasswordLogin";
 import routes from "../routes/Routes";
 import { useLoginStep, useEmail } from "../stores/login";
+import { toast } from "react-toastify";
 import { usePostRequest } from "../services/api"; // Import the usePostRequest hook from your API configuration
 
 const Login = () => {
@@ -22,49 +23,81 @@ const Login = () => {
 
     // Use the submitEmail mutation to post the email
     submitEmail(
-      { url: "/send-otp", data: { email } },
+      { url: "/auth/send-otp", data: { email } },
       {
         onSuccess: (data) => {
-          console.log("API Response Data:", data);
+          // console.log("API Response Data:", data);
           setUserExists(data.exists);
           setStep("verification"); // Move to the next step
+          toast.success("کد ورود به ایمیل شما ارسال شد.");
         },
         onError: (error) => {
           console.error("Error checking email:", error);
-          alert("Error checking email. Please try again.");
+          // toast.error("Error checking email. Please try again.");
+          if (error.response?.data?.message) {
+            toast.error(error.response.data.message);
+          } else {
+            toast.error("مشکلی رخ داده است. دوباره تلاش کنید.");
+          }
         },
       }
     );
   };
 
-  // Use the usePostRequest hook for OTP submission
   const { mutate: submitOTP, isLoading: isSubmittingOTP } = usePostRequest();
 
   const handleVerificationSuccess = (otp) => {
     console.log("Submitting OTP:", { email, otp }); // Debugging
 
-    // Use the submitOTP mutation to post the OTP
     submitOTP(
-      { url: "/verify-otp", data: { email : email, otp: otp } },
+      { url: "/auth/login/otp", data: { email : email, otp: otp } },
       {
-        onSuccess: () => {
-          // In both login and registration modes should set token and operations required
+        onSuccess: (response) => {
+
+          console.log("proccess running ...");
+          
+          const authHeader = response.headers["Authorization"];
+          if (authHeader && authHeader.startsWith("Bearer ")) {
+            const token = authHeader.split(" ")[1];
+            localStorage.setItem("jwtToken", token);
+            console.log("JWT Token:", token);
+          } else {
+            console.error("Authorization header missing or invalid");
+          }
+
+          // // localStorage.setItem("test", Cookies.get('.Tunnels.Relay.WebForwarding.Cookies'));
+          // localStorage.setItem("jwtToken", localStorage.get('jwtToken'));
+          // console.log("jwt : ", localStorage.get('jwtToken'));
+          // // console.log("test Token : ", Cookies.get('.Tunnels.Relay.WebForwarding.Cookies'));
+          // console.log("proccess ended");
+          console.log(response);
+          console.log(response.headers);
+
+
           if (userExists) {
             navigate(routes.home);
+            toast.success("خوش آمدید.");
           } else {
             navigate(routes.completeProfile);
+            toast.success("ورود موفقیت آمیز بود. پروفایل خود را تکمیل کنید.");
           }
         },
         onError: (error) => {
           console.error("Error checking OTP:", error);
-          alert("Error checking OTP. Please try again.");
+          // alert("Error checking OTP. Please try again.");
+          if (error.response?.data?.message) {
+            toast.error(error.response.data.message);
+          } else {
+            console.log("cuse : " , error.cause);
+            console.log("error : " , error);
+            toast.error("مشکلی رخ داده است. دوباره تلاش کنید.");
+          }
         },
       }
     );
   };
 
   const handlePasswordLoginSuccess = () => {
-    // Perform the operations required for entry
     navigate(routes.home);
   };
 
@@ -79,8 +112,10 @@ const Login = () => {
         )}
         {step === "verification" && (
           <VerificationCode
+            handleEmailSubmit={handleEmailSubmit}
             handleVerificationSuccess={handleVerificationSuccess}
             userExists={userExists}
+            isLoading={isSubmittingOTP}
           />
         )}
         {step === "password" && (
